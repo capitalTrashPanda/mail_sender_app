092ee5f56c69961d5b54c204b4405a05930e58e3
Weihao <126637914+capitalTrashPanda@users.noreply.github.com>
2024-07-13 20:12:55 +0800

    feat(logging): format to display only h:m:s

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ log.setLevel(logging.ERROR)
 logging.basicConfig(
     filename="app.log",
     level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(message)s",
+    format="%(asctime)s - %(message)s",
+    datefmt="%H:%M:%S",
 )
 
 
